9167a6f30786df3bad431436b640d28426923b18
Benjamin Bengfort <benjamin@bengfort.com>
2023-03-13 13:41:51 -0500

    add credentials struct

---
+++ credentials/credentials.go
@@ -0,0 +1,30 @@
+package credentials
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+type Credentials struct {
+	accessToken string
+	insecure    bool
+}
+
+func (t *Credentials) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"Authorization": "Bearer " + t.accessToken,
+	}, nil
+}
+
+func (t *Credentials) RequireTransportSecurity() bool {
+	return !t.insecure
+}
+
+func PerRPCToken(accessToken string, insecure bool) grpc.CallOption {
+	return grpc.PerRPCCredentials(&Credentials{accessToken: accessToken, insecure: insecure})
+}
+
+func WithPerRPCToken(accessToken string, insecure bool) grpc.DialOption {
+	return grpc.WithPerRPCCredentials(&Credentials{accessToken: accessToken, insecure: insecure})
+}
