c40a229f2d6f2534560130f9515c22e403197817
Benjamin Bengfort <benjamin@bengfort.com>
2023-03-21 13:11:54 -0500

    Better Exists Check and Topic Name Lookup

--- topics.go
+++ topics.go
@@ -2,28 +2,23 @@ package sdk
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 
 	"github.com/oklog/ulid/v2"
 	api "github.com/rotationalio/go-ensign/api/v1beta1"
+	"github.com/spaolacci/murmur3"
 )
 
 const DefaultPageSize uint32 = 100
 
 // Check if a topic with the specified name exists in the project or not.
 func (c *Client) TopicExists(ctx context.Context, topicName string) (_ bool, err error) {
-	// TODO: more directly use an exists RPC call for faster checking with an index.
-	var topics []*api.Topic
-	if topics, err = c.ListTopics(ctx); err != nil {
+	var info *api.TopicExistsInfo
+	if info, err = c.api.TopicExists(ctx, &api.TopicName{Name: topicName}); err != nil {
 		return false, err
 	}
-
-	for _, topic := range topics {
-		if topic.Name == topicName {
-			return true, nil
-		}
-	}
-	return false, nil
+	return info.Exists, nil
 }
 
 // Create topic with the specified name and return the topic ID if there was no error.
@@ -80,4 +75,32 @@ func (c *Client) ArchiveTopic(ctx context.Context, topicID string) (err error) {
 // Destroy a topic removing it and all of its data.
 func (c *Client) DestroyTopic(ctx context.Context, topicID string) (err error) {
 	return errors.New("not implemented yet")
+}
+
+// Find a topic ID from a topic name.
+// TODO: automate and cache this on the client for easier lookups.
+func (c *Client) TopicID(ctx context.Context, topicName string) (_ string, err error) {
+	// Create a base64 encoded murmur3 hash of the topic name
+	hash := murmur3.New128()
+	hash.Write([]byte(topicName))
+	topicHash := base64.RawURLEncoding.EncodeToString(hash.Sum(nil))
+
+	// List the topic names until the topic ID is found
+	var page *api.TopicNamesPage
+	query := &api.PageInfo{PageSize: uint32(100)}
+
+	for page != nil && query.NextPageToken != "" {
+		if page, err = c.api.TopicNames(ctx, query); err != nil {
+			return "", err
+		}
+
+		for _, topic := range page.TopicNames {
+			if topic.Name == topicHash {
+				return topic.TopicId, nil
+			}
+		}
+		query.NextPageToken = page.NextPageToken
+	}
+
+	return "", ErrTopicNameNotFound
 }
