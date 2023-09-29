5089f918e6c9b40b229efa4f035dfba1f1cfe540
Benjamin Bengfort <benjamin@bengfort.com>
2023-09-29 08:48:31 -0700

    version bump v0.10.0

--- version.go
+++ version.go
@@ -5,10 +5,10 @@ import "fmt"
 // Version component constants for the current build.
 const (
 	VersionMajor         = 0
-	VersionMinor         = 9
+	VersionMinor         = 10
 	VersionPatch         = 0
 	VersionReleaseLevel  = "beta"
-	VersionReleaseNumber = 8
+	VersionReleaseNumber = 9
 )
 
 // Version returns the semantic version for the current build.
